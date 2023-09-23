7c232025a46b7d3ef5e2916a886ae9226c4fd121
aylin <aylindemetci@gmail.com>
2023-09-23 17:08:19 -0600

    interface testing

--- ChatBot/src/dialogflowAuth.js
+++
@@ -1,9 +0,0 @@
-const dialogflow = require('dialogflow');
-
-const sessionClient = new dialogflow.SessionsClient({
-  keyFilename: 'path/to/your/service-account-key.json',
-});
-
-const sessionPath = sessionClient.sessionPath('your-project-id', 'unique-session-id');
-
-export { sessionClient, sessionPath };
